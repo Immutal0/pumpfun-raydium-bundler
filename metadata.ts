3af1ff1dc0ec45857412a4068ce523c1f3e1b3b4
Immutal0 <lovemomdev327@gmail.com>
2025-02-27 16:19:27 +0900

    Feat: update

---
+++ metadata.ts
@@ -0,0 +1,13 @@
+const metadata = {
+    "name": "Immutal0 Token",
+    "symbol": "IMMUTAL0",
+    "description": "Pump Fun Immutal0 Token",
+    "image": "./upload/img.png",
+    "showName": true,
+    "createdOn": "https://pump.fun",
+    "twitter": "https://x.com/Immutal0_",
+    "telegram": "https://t.me/Immutal0",
+    "website": "https://github.com/Immutal0"
+}
+
+export default metadata;
